21e08238235699171a16211f074d8da75ac6d7b8
Hema Malik <Hema Malik@DESKTOP-5AT2IQ6>
2020-07-10 08:38:11 +0530

    Add package to call API with class "CallSendBoxApi". Add package to call class "PayWithRewardsClient" in main package.

---
+++ cuckoo/currency-issuer/amex/amex-api-sample/amex-api-java-client-core-master/src/main/java/io/aexp/api/client/callapi/CallSendBoxApi.java
@@ -0,0 +1,27 @@
+package io.aexp.api.client.callapi;
+
+import java.io.IOException;
+
+import io.aexp.api.client.sample.PayWithRewardsClient;
+
+public class CallSendBoxApi {
+	
+	public static final String payWithRewards = "{\r\n" + 
+			"  \"account_key\": {\r\n" + 
+			"    \"card_number\": \"379620156962001\"\r\n" + 
+			"  },\r\n" + 
+			"  \"amount\": {\r\n" + 
+			"    \"value\": \"10\",\r\n" + 
+			"    \"currency_code\": \"USD\"\r\n" + 
+			"  }\r\n" + 
+			"}";
+
+	public static void main(String[] args) throws IOException {
+		PayWithRewardsClient payWithRewardsClient = new PayWithRewardsClient();
+
+		payWithRewardsClient.searchRewards(payWithRewards);
+		
+		System.out.println("PayWithRewards " + payWithRewards);
+	}
+
+}
